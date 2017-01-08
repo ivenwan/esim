51c666ea41b9baeac636edf82697da1a9623ed6b
Lu Wan <ivenwan@gmail.com>
2017-01-07 16:38:50 -0800

    Add argument class and trigger class

---
+++ argument.py
@@ -0,0 +1,47 @@
+class Argument(object):
+    """ This is a class for passing arguments
+    """
+
+    def __init__(self):
+        self.arglist = {} # empty dict
+    
+    def insert(self, argname):
+        """ insert an argname with no argval (True)
+        """
+        self.insert(argname, True)
+
+    def insert(self, argname, argval):
+        """ insert an arg dict entry {argname:argval}
+
+        """
+        self.arglist[argname] = argval
+
+    def remove(self, argname):
+        del self.arglist[argname]
+
+    def get(self,argname):
+        """ no argname -> None
+            otherwise, return the argval
+        """
+        return self.arglist[argname]
+    
+    def has(self, argname):
+        """check if contain an argname
+        """
+        if (sef.arglist[argname] == None):
+            return False
+        return True 
+
+    def __repr__(self):
+        str = ''
+        for argname in self.arglist:
+            str = str + "[%s: %s]\n" % (argname, self.arglist[argname])
+        return str
+
+
+# test
+test_args = Argument()
+test_args.insert('id', 5)
+test_args.insert('parent', 'John')
+
+print test_args
---
+++ trigger.py
@@ -0,0 +1,47 @@
+from argument import Argument
+
+class Trigger(object):
+    """ This is a class for trigger:
+        A trigger consists of:
+        - a master
+        - a slave
+        - Master trigger condition
+        - Slave action/function call
+        - Argument passed from Master to Slave for the function call
+    """
+
+    def __init__(self):
+        self.master = None
+        self.slave = None
+        self.trigger = None # master trigger condition
+        self.action = None # slave's function call
+        self.arguments = Argument() # create an arg object
+
+    def set_master(self,master):
+        self.master = master
+
+    def set_slave(self,slave):
+        self.slave = slave
+
+    def set_trigger(self,trigger):
+        self.trigger = trigger
+
+    def add_argument(self, argname, argval):
+        self.arguments.insert(argname, argval)
+
+    def new_trigger(self, master, slave, trigger):
+        self.set_master(master)
+        self.set_slave(slave)
+        self.set_trigger(trigger)
+    
+    def __repr__(self):
+        str = ''
+        str = str + "%s triggers %s upon %s with arg %s" % (self.master, self.slave, self.trigger, self.arguments) 
+        return str
+
+# test
+test_trigger = Trigger()
+test_trigger.add_argument("sharability", "inner_sharable")
+test_trigger.new_trigger("CacheControl", "L1DCache", "data_arrival")
+
+print test_trigger
